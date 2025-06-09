b2e63b597c7ae0763d84324e494284044e593094
Mia Pilchová <pilchova@lilianaa.dev>
2025-06-09 08:10:19 +0200

    allow customization and add Vue support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,39 +5,88 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import vue from "eslint-plugin-vue";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.eslintRecommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
+/**
+ * @type {import('typescript-eslint').ConfigWithExtends["rules"]}
+ */
+const BASE_RULES = {
+  "no-unused-vars": ["off"],
+  "@typescript-eslint/no-unused-vars": [
+    "error",
+    {
+      varsIgnorePattern: "^_",
+      argsIgnorePattern: "^_",
     },
-    rules: {
-      "no-unused-vars": ["off"],
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          varsIgnorePattern: "^_",
-          argsIgnorePattern: "^_",
-        },
-      ],
-      "@typescript-eslint/no-explicit-any": "warn",
-      "no-empty": ["warn", { allowEmptyCatch: true }],
-      "simple-import-sort/imports": "error",
-      "@typescript-eslint/no-empty-function": "off",
-      "import/no-anonymous-default-export": "off",
-      "@typescript-eslint/ban-ts-comment": "warn",
-      "no-useless-escape": "off",
+  ],
+  "@typescript-eslint/no-explicit-any": "warn",
+  "no-empty": ["warn", { allowEmptyCatch: true }],
+  "simple-import-sort/imports": "error",
+  "@typescript-eslint/no-empty-function": "off",
+  "import/no-anonymous-default-export": "off",
+  "@typescript-eslint/ban-ts-comment": "warn",
+  "no-useless-escape": "off",
+}
+
+/**
+ * @type {import('typescript-eslint').ConfigWithExtends["rules"]}
+ */
+const VUE_RULES = {
+  "vue/multi-word-component-names": "off",
+  "vue/first-attribute-linebreak": "off",
+  "vue/max-attributes-per-line": "off",
+  "vue/no-reserved-component-names": [
+    "error",
+    {
+      htmlElementCaseSensitive: true,
     },
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-        ...globals.node,
+  ],
+  "vue/html-self-closing": [
+    "error",
+    {
+      html: {
+        void: "always",
       },
     },
-    ignores: ["node_modules/"],
-  },
-);
+  ],
+};
+
+export function createConfig({
+  vue: useVue = false,
+  ignores = ["node_modules", "dist"],
+  globals: globalsKeys = ["jest", "node"]
+} = {}) {
+  if (useVue) {
+    globalsKeys.push("browser");
+  }
+
+  globalsKeys = [...new Set(globalsKeys)];
+
+  return tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.eslintRecommended,
+    ...tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+    ...(useVue ? vue.configs["flat/recommended"] : []),
+    {
+      plugins: {
+        "simple-import-sort": simpleImportSort,
+      },
+      rules: {
+        ...BASE_RULES,
+        ...(useVue ? VUE_RULES : {}),
+      },
+      languageOptions: {
+        globals: globalsKeys.reduce((acc, curr) => {
+          return Object.assign(acc, globals[curr])
+        }, {}),
+      },
+      ignores: [...new Set(ignores)],
+    },
+  );
+  
+}
+
+const defaultConfig = createConfig()
+
+export default defaultConfig;
