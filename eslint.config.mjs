5d66d6ed1f9002034e49c24eb2bf995da3e0255f
Mia Pilchová <pilchova@lilianaa.dev>
2025-12-26 18:15:18 +0100

    refactor for future extensibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -51,22 +51,83 @@ const VUE_RULES = {
   ],
 };
 
+const CONFIGS = {
+  vue: {
+    parser: "@typescript-eslint/parser",
+    ignores: [],
+    globals: ["browser"],
+    rules: VUE_RULES,
+  },
+};
+
 export function createConfig({
-  vue: useVue = false,
+  /** @deprecated Use `include: ["vue"]` instead. */
+  vue: legacyUseVue = false,
+  include = /** @type {string[]} */ ([]),
   ignores = ["node_modules", "dist"],
   globals: globalsKeys = ["jest", "node"],
 } = {}) {
-  if (useVue) {
-    globalsKeys.push("browser");
+  const includesVue = include.includes("vue");
+
+  if (legacyUseVue) {
+    const refactorSuggestion = includesVue
+      ? "Remove it from your config options."
+      : 'Please use `include: ["vue"]` instead.';
+
+    console.warn(`[@lilianaa/configs] The "vue" option is deprecated. ${refactorSuggestion}`);
+
+    if (!includesVue) {
+      include.push("vue");
+    }
   }
 
+  // Deduplicate includes.
+  include = [...new Set(include)];
+
+  const mergedIncludedConfig = include
+    // Filter for known configs only.
+    .filter((key) => {
+      if (!(key in CONFIGS)) {
+        console.warn(`[@lilianaa/configs] Unknown config "${key}" included.`);
+        return false;
+      }
+
+      return true;
+    })
+    // Get the config objects.
+    .map((key) => {
+      // @ts-expect-error TS can't infer that key is in CONFIGS
+      return CONFIGS[key];
+    })
+    // Merge them together.
+    .reduce(
+      (acc, curr) => {
+        acc.parser = curr.parser || acc.parser;
+        acc.ignores.push(...curr.ignores);
+        acc.globals.push(...curr.globals);
+        Object.assign(acc.rules, curr.rules);
+
+        return acc;
+      },
+      {
+        parser: null,
+        ignores: [],
+        globals: [],
+        rules: {},
+      },
+    );
+
+  // Merge config globals and include globals.
+  globalsKeys = [...globalsKeys, ...mergedIncludedConfig.globals];
+  // Deduplicate globals.
   globalsKeys = [...new Set(globalsKeys)];
 
   return tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.eslintRecommended,
     ...tseslint.configs.recommended,
-    ...(useVue ? vue.configs["flat/recommended"] : []),
+    // FIXME: This should be part of VUE_RULES, but it needs to be applied earlier.
+    ...(includesVue ? vue.configs["flat/recommended"] : []),
     eslintPluginPrettierRecommended,
     {
       plugins: {
@@ -74,13 +135,19 @@ export function createConfig({
       },
       rules: {
         ...BASE_RULES,
-        ...(useVue ? VUE_RULES : {}),
+        ...mergedIncludedConfig.rules,
       },
       languageOptions: {
         parserOptions: {
-          ...(useVue ? { parser: "@typescript-eslint/parser" } : {}),
+          ...(mergedIncludedConfig.parser ? { parser: mergedIncludedConfig.parser } : {}),
         },
         globals: globalsKeys.reduce((acc, curr) => {
+          if (!(curr in globals)) {
+            console.warn(`[@lilianaa/configs] Unknown global "${curr}" specified.`);
+            return acc;
+          }
+
+          // @ts-expect-error TS can't infer that curr is in globals
           return Object.assign(acc, globals[curr]);
         }, {}),
       },
